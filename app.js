13d000666aa0165109334df9f8816303cd192ff6
Ferresse <61714051+Ferresse@users.noreply.github.com>
2022-07-04 21:38:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ var hookMessage = document.getElementById("message");
 var hookAvatar = document.getElementById("avatarUrl");
 var hookCount = document.getElementById("count");
 var hookDelay = document.getElementById("delay");
+let i = 0;
 
 startButton.addEventListener("click", () => {
     if (hookCount.value != 0 && hookUrl.value != 0 && hookMessage.value != 0) {
-        for (let i = 0; i < hookCount.value; i++) {
-            let delayInterval = setInterval(() => {
-                console.log("x", i);
-                // fetch(hookUrl.value, {
-                //     method: "POST",
-                //     headers: {
-                //         "Content-Type": "application/json",
-                //     },
-                //     body: JSON.stringify({
-                //         username: hookName.value || "hooker",
-                //         avatar_url: hookAvatar.value || "",
-                //         content: hookMessage.value || ".",
-                //     }),
-                // });
-                if (i === hookCount.value) {
-                    clearInterval(delayInterval);
-                }
-            }, 500);
+        function increment() {
+            if (i < hookCount.value) {
+                i++;
+                console.log(i);
+                const fetchRespone = fetch(hookUrl.value, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username: hookName.value || "hooker",
+                        avatar_url: hookAvatar.value || "",
+                        content: hookMessage.value || ".",
+                    }),
+                });
+            } else {
+                clearInterval(incrementInterval);
+                i = 0;
+            }
         }
     } else {
         alert("Put values in");
     }
+    console.log(fetchRespone);
+    let incrementInterval = setInterval(increment(), hookDelay.value || 500);
 });
